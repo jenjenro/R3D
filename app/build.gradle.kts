857e70cffbdb0207a11706f1712c248033825bc5
David Gutierrez <1608davidg@gmail.com>
2024-05-08 15:01:40 -0500

    Conexion con la firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,6 +68,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.activity)
+    implementation(libs.firebase.firestore.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/test/r3d/Home.kt
+++
@@ -1,1 +0,0 @@
-package com.test.r3d
