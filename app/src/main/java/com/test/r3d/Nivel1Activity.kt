7865f3d479a53a68d8ea2db6f8a8f0181509e6db
David Gutierrez <1608davidg@gmail.com>
2024-05-29 21:59:12 -0500

    Logica de las preguntas

--- app/src/main/java/com/test/r3d/Nivel1Activity.kt
+++ app/src/main/java/com/test/r3d/Nivel1Activity.kt
@@ -2,58 +2,108 @@ package com.test.r3d
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.ArrayMap
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import com.google.firebase.firestore.ktx.firestore
-import com.google.firebase.ktx.Firebase
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.tasks.await
 import java.util.ArrayList
-import kotlin.math.absoluteValue
-
+import java.util.Random
 
 class Nivel1Activity : AppCompatActivity() {
-    private var index:Int=0
-    private var index2:Int=0
+    private var correct: Int = 0
+    private var index: Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.nivel1)
-        var question :Question = Question("module5")
-
-        val textStatement  = findViewById<TextView>(R.id.textStatement)
-        val btn = findViewById<Button>(R.id.button19)
-
+        var question = Question("module5")
         val preguntas: ArrayList<ArrayList<String>> = ArrayList()
-        val enunciado: ArrayList<String> = ArrayList()
-        for ( i in 1..5) {
+        val btnOpcion1 = findViewById<Button>(R.id.buttonOption1)
+        val btnOpcion2 = findViewById<Button>(R.id.buttonOption2)
+        val btnOpcion3 = findViewById<Button>(R.id.buttonOption3)
+
+        for (i in 1..5) {
             val questions = question.createQuestion(i)
-            val statements = question.createStatements(i)
-            statements.forEach { statement ->
-                enunciado.add(statement.value as String)
-            }
+
             questions.forEach { question ->
                 preguntas.add(question.value as ArrayList<String>)
             }
         }
-        textStatement.text = preguntas[index][0]
-        btn.setOnClickListener {
-            index2++;
-            index++;
-            if(index<preguntas.size) {
-                textStatement.text = preguntas[index][0]
 
-            } else if (index>=preguntas.size) {
-                val intent = Intent(this, Module1Activity::class.java)
-                startActivity(intent)
+        correctQuestion(preguntas, index)
+
+        btnOpcion1.setOnClickListener {
+            if (btnOpcion1.text == preguntas[index][1]) {
+                correct++
+                index++
+                correctQuestion(preguntas, index)
+            }
+        }
+        btnOpcion2.setOnClickListener {
+            if (btnOpcion2.text == preguntas[index][1]) {
+                correct++
+                index++
+                correctQuestion(preguntas, index)
+            }
+        }
+        btnOpcion3.setOnClickListener {
+            if (btnOpcion3.text == preguntas[index][1]) {
+                correct++
+                index++
+                correctQuestion(preguntas, index)
+            }
+        }
+    }
+
+    fun correctQuestion(preguntas: ArrayList<ArrayList<String>> = ArrayList(), index: Int) {
+        val numRandoms: ArrayList<Int> = ArrayList()
+        val numRandoms2: ArrayList<Int> = ArrayList()
+        val textStatement = findViewById<TextView>(R.id.textStatement)
+        val btnOpcion1 = findViewById<Button>(R.id.buttonOption1)
+        val btnOpcion2 = findViewById<Button>(R.id.buttonOption2)
+        val btnOpcion3 = findViewById<Button>(R.id.buttonOption3)
+
+        if (index < preguntas.size) {
+            println(preguntas[index].size.minus(1))
+            if (preguntas[index].size.minus(1) == 2) {
+                do {
+                    val randomNumber = (1..preguntas[index].size.minus(1)).random()
+                    if (randomNumber !in numRandoms) {
+                        numRandoms.add(randomNumber)
+                    }
+                } while (numRandoms.size !== 2)
+            } else{
+                do {
+                    val randomNumber = (1..preguntas[index].size.minus(1)).random()
+                    if (randomNumber !in numRandoms) {
+                        numRandoms.add(randomNumber)
+                    }
+                } while (numRandoms.size !== 3)
             }
 
+            println(numRandoms)
+            if (1 !in numRandoms) {
+                val randomNumber = (1..3).random()
+                numRandoms[randomNumber] = 1
+            }
+            println(numRandoms)
 
+            if (index == 1) {
+                btnOpcion3.visibility = Button.INVISIBLE
+                textStatement.text = preguntas[index][0]
+                btnOpcion1.text = preguntas[index][numRandoms[0]]
+                btnOpcion2.text = preguntas[index][numRandoms[1]]
+            } else {
+                btnOpcion3.visibility = Button.VISIBLE
+                textStatement.text = preguntas[index][0]
+                btnOpcion1.text = preguntas[index][numRandoms[0]]
+                btnOpcion2.text = preguntas[index][numRandoms[1]]
+                btnOpcion3.text = preguntas[index][numRandoms[2]]
+
+            }
+
+        } else if (index >= preguntas.size) {
+            val intent = Intent(this, Module1Activity::class.java)
+            startActivity(intent)
         }
-
-
     }
 }
