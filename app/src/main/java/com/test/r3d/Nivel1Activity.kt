27b5d57812c65cd80fc6abb5b6f942c6bcd55bc8
Jenny Rojas <rojasor219@gmail.com>
2024-06-04 14:12:38 -0500

    Dialogs listos y activacion del dialog listo para implementar en clase Nivel1Activity.kt

--- app/src/main/java/com/test/r3d/Nivel1Activity.kt
+++ app/src/main/java/com/test/r3d/Nivel1Activity.kt
@@ -4,31 +4,43 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
-import android.window.OnBackInvokedDispatcher
-import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
+import java.util.ArrayList
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+
+
 class Nivel1Activity : AppCompatActivity() {
     private var correct: Int = 0
     private var index: Int = 0
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
         setContentView(R.layout.nivel1)
-
-        onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true){
-            override fun handleOnBackPressed() {
-
-            }
-        })
-
-        val question = Question("module1")
-        val statement: ArrayList<String> = ArrayList()
+        var question = Question("module5")
         val preguntas: ArrayList<ArrayList<String>> = ArrayList()
-        val btnOpcion1 = findViewById<Button>(R.id.btnOp1lv1)
-        val btnOpcion2 = findViewById<Button>(R.id.btnOp2lv1)
-        val btnOpcion3 = findViewById<Button>(R.id.btnOp3lv1)
+        val btnOpcion1 = findViewById<Button>(R.id.buttonOption1)
+        val btnOpcion2 = findViewById<Button>(R.id.buttonOption2)
+        val btnOpcion3 = findViewById<Button>(R.id.buttonOption3)
+
+        val btnD = findViewById<Button>(R.id.buttonOption1)
+
+        dialog()
+
+        btnD.setOnClickListener{
+            val dialogBinding = layoutInflater.inflate(R.layout.dialog_ok,null)
+
+            val myDialog = android.app.Dialog(this)
+            myDialog.setContentView(dialogBinding)
+
+            myDialog.setCancelable(true)
+            myDialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+            myDialog.show()
+            }
+
+
+
+
 
         for (i in 1..5) {
             val questions = question.createQuestion(i)
@@ -36,46 +48,54 @@ class Nivel1Activity : AppCompatActivity() {
             questions.forEach { question ->
                 preguntas.add(question.value as ArrayList<String>)
             }
-            val statements = question.createStatements(i)
-            statements.forEach { i ->
-                statement.add(i.value as String)
-            }
         }
 
-        correctQuestion(preguntas, index, statement)
+        correctQuestion(preguntas, index)
 
         btnOpcion1.setOnClickListener {
             if (btnOpcion1.text == preguntas[index][1]) {
                 correct++
                 index++
-                correctQuestion(preguntas, index, statement)
+                correctQuestion(preguntas, index)
             }
         }
         btnOpcion2.setOnClickListener {
             if (btnOpcion2.text == preguntas[index][1]) {
                 correct++
                 index++
-                correctQuestion(preguntas, index, statement)
+                correctQuestion(preguntas, index)
             }
         }
         btnOpcion3.setOnClickListener {
             if (btnOpcion3.text == preguntas[index][1]) {
                 correct++
                 index++
-                correctQuestion(preguntas, index, statement)
+                correctQuestion(preguntas, index)
             }
         }
     }
 
-    private fun correctQuestion(preguntas: ArrayList<ArrayList<String>> = ArrayList(), index: Int, statements: ArrayList<String> = ArrayList()) {
+    private fun dialog(){
+        val dialogBinding = layoutInflater.inflate(R.layout.dialog_ok,null)
+
+        val myDialog = android.app.Dialog(this)
+        myDialog.setContentView(dialogBinding)
+
+        myDialog.setCancelable(true)
+        myDialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        myDialog.show()
+    }
+
+    fun correctQuestion(preguntas: ArrayList<ArrayList<String>> = ArrayList(), index: Int) {
         val numRandoms: ArrayList<Int> = ArrayList()
+        val numRandoms2: ArrayList<Int> = ArrayList()
         val textStatement = findViewById<TextView>(R.id.textStatement)
-        val textStatement1 = findViewById<TextView>(R.id.textStatement1)
-        val btnOpcion1 = findViewById<Button>(R.id.btnOp1lv1)
-        val btnOpcion2 = findViewById<Button>(R.id.btnOp2lv1)
-        val btnOpcion3 = findViewById<Button>(R.id.btnOp3lv1)
+        val btnOpcion1 = findViewById<Button>(R.id.buttonOption1)
+        val btnOpcion2 = findViewById<Button>(R.id.buttonOption2)
+        val btnOpcion3 = findViewById<Button>(R.id.buttonOption3)
 
         if (index < preguntas.size) {
+            println(preguntas[index].size.minus(1))
             if (preguntas[index].size.minus(1) == 2) {
                 do {
                     val randomNumber = (1..preguntas[index].size.minus(1)).random()
@@ -92,21 +112,21 @@ class Nivel1Activity : AppCompatActivity() {
                 } while (numRandoms.size !== 3)
             }
 
+            println(numRandoms)
             if (1 !in numRandoms) {
-                val randomNumber = (0..2).random()
+                val randomNumber = (1..3).random()
                 numRandoms[randomNumber] = 1
             }
+            println(numRandoms)
 
-            if (index == 2) {
+            if (index == 1) {
                 btnOpcion3.visibility = Button.INVISIBLE
-                textStatement.text = statements[index]
-                textStatement1.text = preguntas[index][0]
+                textStatement.text = preguntas[index][0]
                 btnOpcion1.text = preguntas[index][numRandoms[0]]
                 btnOpcion2.text = preguntas[index][numRandoms[1]]
             } else {
                 btnOpcion3.visibility = Button.VISIBLE
-                textStatement.text = statements[index]
-                textStatement1.text = preguntas[index][0]
+                textStatement.text = preguntas[index][0]
                 btnOpcion1.text = preguntas[index][numRandoms[0]]
                 btnOpcion2.text = preguntas[index][numRandoms[1]]
                 btnOpcion3.text = preguntas[index][numRandoms[2]]
@@ -119,4 +139,3 @@ class Nivel1Activity : AppCompatActivity() {
         }
     }
 }
-
