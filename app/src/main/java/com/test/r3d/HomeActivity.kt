a6510e35e0420a8a8c1903465dd164d2bdfbc5aa
Jenny Rojas <rojasor219@gmail.com>
2024-06-03 22:24:57 -0500

    intento tener lo mismo del branch oscar

--- app/src/main/java/com/test/r3d/HomeActivity.kt
+++ app/src/main/java/com/test/r3d/HomeActivity.kt
@@ -13,23 +13,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.test.r3d.ui.theme.R3DTheme
-import android.view.Window
-import android.app.Dialog
-import android.graphics.Color
-import android.graphics.drawable.ColorDrawable
-import android.widget.Toast
-
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.home)
-
-        val btnSMessage : Button = findViewById(R.id.btnNextScreen)//.boton>equivocado
-        btnSMessage.setOnClickListener {
-            val message : String? = "No te rindas!"
-            showCustomDialogBox(message)
-        }
 
 
         val btnPrueba = findViewById<Button>(R.id.btnNextScreen)
@@ -39,22 +27,4 @@ class MainActivity : ComponentActivity() {
         }
 
     }
-
-    private fun showCustomDialogBox(message: String?) {
-        val dialog = Dialog(this)
-        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
-        dialog.setCancelable(false)
-        dialog.setContentView(R.layout.dailog_ups)
-        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
-
-        val tvMessage: Button = dialog.findViewById(R.id.btnBlue)
-        val btnSMessage : Button = dialog.findViewById(R.id.sMessage)
-        tvMessage.text = message
-
-        btnSMessage.setOnClickListener{
-            Toast.makeText(this, "Click on Seguir intentanto", Toast.LENGTH_LONG).show()
-        }
-        dialog.show()
-    }
-
 }
