549800e5425fbbd96b3430bd17e96f12a8bc728c
David Gutierrez <1608davidg@gmail.com>
2024-06-11 16:35:17 -0500

    Bluetooth

--- app/src/main/java/com/test/r3d/ControlActivity.kt
+++ app/src/main/java/com/test/r3d/ControlActivity.kt
@@ -1,14 +1,69 @@
 package com.test.r3d
 
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothManager
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import java.util.UUID
 
+const val REQUEST_ENABLE_BT = 1
 class ControlActivity : AppCompatActivity() {
+    lateinit var mBtAdapter: BluetoothAdapter
+    var mAddress: String? = null
+    var mName: String? = null
+
+    companion object{
+        var m_myUUID: UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB")
+        private var m_bluetoothSocket: BluetoothAdapter? = null
+        var m_isConnected: Boolean = false
+        lateinit var m_address: String
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.control)
+        val btnUp = findViewById<Button>(R.id.btnUp)
+        val btnDown = findViewById<Button>(R.id.btnDown)
+        val btnLeft = findViewById<Button>(R.id.btnLeft)
+        val btnRight = findViewById<Button>(R.id.btnRight)
+        val btnOn = findViewById<Button>(R.id.btnOn)
 
+        val someActivityResultLauncher = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ) { result ->
+            if (result.resultCode == REQUEST_ENABLE_BT) {
+                Log.i("MainActivity",("Bluetooth has been enabled"))
+            }
+        }
 
+        mBtAdapter = (getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager).adapter
+
+        if (mBtAdapter == null) {
+            Toast.makeText(this, "Bluetooth is not available", Toast.LENGTH_LONG).show()
+        } else{
+            Toast.makeText(this, "Bluetooth is available", Toast.LENGTH_LONG).show()
+        }
+
+        btnOn.setOnClickListener {
+            if (mBtAdapter.isEnabled) {
+                Toast.makeText(this, "Bluetooth is already on", Toast.LENGTH_SHORT).show()
+            } else {
+                val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+                if (ActivityCompat.checkSelfPermission(this,
+                        android.Manifest.permission.BLUETOOTH_CONNECT
+                ) != PackageManager.PERMISSION_GRANTED){
+                    ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.BLUETOOTH_CONNECT), REQUEST_ENABLE_BT)
+                }
+                someActivityResultLauncher.launch(enableBtIntent)
+            }
+        }
     }
 }
--- app/src/main/java/com/test/r3d/Nivel1Activity.kt
+++ app/src/main/java/com/test/r3d/Nivel1Activity.kt
@@ -1,9 +1,13 @@
 package com.test.r3d
 
+import android.bluetooth.BluetoothAdapter
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import android.window.OnBackInvokedDispatcher
+import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
 import java.util.ArrayList
 import android.graphics.Color
@@ -14,33 +18,24 @@ class Nivel1Activity : AppCompatActivity() {
     private var correct: Int = 0
     private var index: Int = 0
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.nivel1)
-        var question = Question("module5")
-        val preguntas: ArrayList<ArrayList<String>> = ArrayList()
-        val btnOpcion1 = findViewById<Button>(R.id.buttonOption1)
-        val btnOpcion2 = findViewById<Button>(R.id.buttonOption2)
-        val btnOpcion3 = findViewById<Button>(R.id.buttonOption3)
 
-        val btnD = findViewById<Button>(R.id.buttonOption1)
+        onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true){
+            override fun handleOnBackPressed() {
 
-        dialog()
-
-        btnD.setOnClickListener{
-            val dialogBinding = layoutInflater.inflate(R.layout.dialog_ok,null)
-
-            val myDialog = android.app.Dialog(this)
-            myDialog.setContentView(dialogBinding)
-
-            myDialog.setCancelable(true)
-            myDialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
-            myDialog.show()
             }
+        })
 
-
-
-
+        val question = Question("module1")
+        val statement: ArrayList<String> = ArrayList()
+        val preguntas: ArrayList<ArrayList<String>> = ArrayList()
+        val btnOpcion1 = findViewById<Button>(R.id.btnOp1lv1)
+        val btnOpcion2 = findViewById<Button>(R.id.btnOp2lv1)
+        val btnOpcion3 = findViewById<Button>(R.id.btnOp3lv1)
 
         for (i in 1..5) {
             val questions = question.createQuestion(i)
@@ -48,51 +43,66 @@ class Nivel1Activity : AppCompatActivity() {
             questions.forEach { question ->
                 preguntas.add(question.value as ArrayList<String>)
             }
+            val statements = question.createStatements(i)
+            statements.forEach { i ->
+                statement.add(i.value as String)
+            }
         }
 
-        correctQuestion(preguntas, index)
+        correctQuestion(preguntas, index, statement)
 
         btnOpcion1.setOnClickListener {
             if (btnOpcion1.text == preguntas[index][1]) {
                 correct++
                 index++
-                correctQuestion(preguntas, index)
+                dialog(preguntas, index, statement, true)
+            } else{
+
             }
         }
         btnOpcion2.setOnClickListener {
             if (btnOpcion2.text == preguntas[index][1]) {
                 correct++
                 index++
-                correctQuestion(preguntas, index)
+                dialog(preguntas, index, statement, true)
             }
         }
         btnOpcion3.setOnClickListener {
             if (btnOpcion3.text == preguntas[index][1]) {
                 correct++
                 index++
-                correctQuestion(preguntas, index)
+                dialog(preguntas, index, statement, true)
             }
         }
     }
 
-    private fun dialog(){
-        val dialogBinding = layoutInflater.inflate(R.layout.dialog_ok,null)
+    private fun dialog(preguntas: ArrayList<ArrayList<String>> = ArrayList(), index: Int, statements: ArrayList<String> = ArrayList(), option : Boolean) {
+        var opt : Int = 0
+        if (option) {
+            opt = R.layout.dialog_ok
+        }
+        val dialogBinding = layoutInflater.inflate(opt ,null)
 
         val myDialog = android.app.Dialog(this)
         myDialog.setContentView(dialogBinding)
-
-        myDialog.setCancelable(true)
+        val btnOk = dialogBinding.findViewById<Button>(R.id.alertDialog)
+        btnOk.setOnClickListener {
+            correctQuestion(preguntas, index, statements)
+            myDialog.dismiss()
+        }
+        myDialog.setCancelable(false)
+        myDialog.setCanceledOnTouchOutside(false)
         myDialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
         myDialog.show()
     }
 
-    fun correctQuestion(preguntas: ArrayList<ArrayList<String>> = ArrayList(), index: Int) {
+    fun correctQuestion(preguntas: ArrayList<ArrayList<String>> = ArrayList(), index: Int, statements: ArrayList<String> = ArrayList()) {
         val numRandoms: ArrayList<Int> = ArrayList()
-        val numRandoms2: ArrayList<Int> = ArrayList()
         val textStatement = findViewById<TextView>(R.id.textStatement)
-        val btnOpcion1 = findViewById<Button>(R.id.buttonOption1)
-        val btnOpcion2 = findViewById<Button>(R.id.buttonOption2)
-        val btnOpcion3 = findViewById<Button>(R.id.buttonOption3)
+        val textStatement1 = findViewById<TextView>(R.id.textStatement1)
+        val btnOpcion1 = findViewById<Button>(R.id.btnOp1lv1)
+        val btnOpcion2 = findViewById<Button>(R.id.btnOp2lv1)
+        val btnOpcion3 = findViewById<Button>(R.id.btnOp3lv1)
 
         if (index < preguntas.size) {
             println(preguntas[index].size.minus(1))
@@ -112,21 +122,21 @@ class Nivel1Activity : AppCompatActivity() {
                 } while (numRandoms.size !== 3)
             }
 
-            println(numRandoms)
             if (1 !in numRandoms) {
-                val randomNumber = (1..3).random()
+                val randomNumber = (0..2).random()
                 numRandoms[randomNumber] = 1
             }
-            println(numRandoms)
 
-            if (index == 1) {
+            if (index == 2) {
                 btnOpcion3.visibility = Button.INVISIBLE
-                textStatement.text = preguntas[index][0]
+                textStatement1.text = preguntas[index][0]
+                textStatement.text = statements[index]
                 btnOpcion1.text = preguntas[index][numRandoms[0]]
                 btnOpcion2.text = preguntas[index][numRandoms[1]]
             } else {
                 btnOpcion3.visibility = Button.VISIBLE
-                textStatement.text = preguntas[index][0]
+                textStatement.text = statements[index]
+                textStatement1.text = preguntas[index][0]
                 btnOpcion1.text = preguntas[index][numRandoms[0]]
                 btnOpcion2.text = preguntas[index][numRandoms[1]]
                 btnOpcion3.text = preguntas[index][numRandoms[2]]
